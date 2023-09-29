bf7d7b050cc3ba4898b58862eed133edf6eaf48d
Nickolay Graysenko <kolabelozorov@gmail.com>
2023-09-29 16:53:05 +0200

    Version2

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    public int rik;
+    public String colour;
+
+    public Car(int rik, String colour) {
+        this.rik = rik;
+        this.colour = colour;
+    }
+
+    public Car(int rik) {
+        this.rik = rik;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        // Створення об'єкту класу Address та ініціалізація полів
-        Addres myAddress = new Addres("12345", "Україна", "Київ", "Вулиця Київська", "42", "23");
-
-        // Виведення інформації про адресу на екран
-        System.out.println("Індекс: " + myAddress.getIndex());
-        System.out.println("Країна: " + myAddress.getCountry());
-        System.out.println("Місто: " + myAddress.getCity());
-        System.out.println("Вулиця: " + myAddress.getStreet());
-        System.out.println("Будинок: " + myAddress.getHouse());
-        System.out.println("Квартира: " + myAddress.getApartment());
+        Car BMW = new Car(1988, "Black");
+        Car Alternado = new Car(2022);
+        System.out.println(BMW.rik);
+        System.out.println(BMW.colour);
+        System.out.println(Alternado.rik);
     }
 }
