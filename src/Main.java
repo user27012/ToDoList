a2b928ab1f03aff92434e2ca01b81aa79ded20fd
Nickolay Graysenko <kolabelozorov@gmail.com>
2023-09-29 19:54:26 +0200

    HW - 04 - 06 final

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Добро пожаловать! Я пощитаю ваши кружочки, пожалуйста укажите радиус ниже:");
-        double radius = scan.nextDouble();
-        System.out.println(myArea.areaOfCircle(radius));
+        Pupil excellentPupil = new ExcellentPupil();
+        Pupil goodPupil = new GoodPupil();
+        Pupil badPupil = new BadPupil();
+        Pupil anotherExcellentPupil = new ExcellentPupil();
 
+        ClassRoom classRoom = new ClassRoom(excellentPupil, goodPupil, badPupil, anotherExcellentPupil);
+        System.out.print("Введите количество учинеков, которых вы бы хотели проверить (от 1 до 4): ");
+        int who = scan.nextInt();
+        switch (who) {
+            case 1:
+                classRoom.showInfo1();
+                break;
+            case 2:
+                classRoom.showInfo2();
+                break;
+            case 3:
+                classRoom.showInfo3();
+                break;
+            case 4:
+                classRoom.showInfo4();
+                break;
+            default:
+                System.out.println("Такого количества учеников нет или не найдено в базе...");
+                break;
+        }
     }
 }
+
+
+
+
+
