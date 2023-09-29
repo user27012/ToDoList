b3edfe2a858a9520f512e90529c837775888c585
Nickolay Graysenko <kolabelozorov@gmail.com>
2023-09-29 19:07:55 +0200

    HW - 04 - 05 final

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        CarOverload BMW = new CarOverload(1999, 190.9, 500, "White");
-        CarOverload lambo = new CarOverload(2017, 280.8, 439);
-        CarOverload Gryzovik = new CarOverload(1932, 88);
-        CarOverload Ferrari = new CarOverload(2022);
-        System.out.println("Основные характеристики машины BMW: Год: " + BMW.rik + " Скорость: " + BMW.speed + " Вес в кг: " + BMW.vaga + " Цвет: " + BMW.colour);
-        System.out.println("Основные характеристики машины lamborgini: Год: " + lambo.rik + " Скорость: " + lambo.speed + " Вес в кг: " + lambo.vaga);
-        System.out.println("Основные характристики грузовика: Год: " + Gryzovik.rik + " Скорость " + Gryzovik.speed);
-        System.out.println("Год выпуска феррари: " + Ferrari.rik);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Добро пожаловать! Я пощитаю ваши кружочки, пожалуйста укажите радиус ниже:");
+        double radius = scan.nextDouble();
+        System.out.println(myArea.areaOfCircle(radius));
+
     }
 }
