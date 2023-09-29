c35a604cffff345cf1cef993a5e8914fb5d6bc4e
Nickolay Graysenko <kolabelozorov@gmail.com>
2023-09-29 21:11:41 +0200

    FinalHomeWork

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,14 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Vehicle vehicle1 = new Plane(50.0, 30.0, 2000000.0, 900.0, 2020, 10000.0, 150);
+        Vehicle vehicle2 = new Car(10.0, 20.0, 50000.0, 120.0, 2019, "Toyota");
+        Vehicle vehicle3 = new Ship(5.0, 15.0, 1000000.0, 50.0, 2015, 200, "Miami");
 
-        Pupil excellentPupil = new ExcellentPupil();
-        Pupil goodPupil = new GoodPupil();
-        Pupil badPupil = new BadPupil();
-        Pupil anotherExcellentPupil = new ExcellentPupil();
-
-        ClassRoom classRoom = new ClassRoom(excellentPupil, goodPupil, badPupil, anotherExcellentPupil);
-        System.out.print("Введите количество учинеков, которых вы бы хотели проверить (от 1 до 4): ");
-        int who = scan.nextInt();
-        switch (who) {
-            case 1:
-                classRoom.showInfo1();
-                break;
-            case 2:
-                classRoom.showInfo2();
-                break;
-            case 3:
-                classRoom.showInfo3();
-                break;
-            case 4:
-                classRoom.showInfo4();
-                break;
-            default:
-                System.out.println("Такого количества учеников нет или не найдено в базе...");
-                break;
-        }
+        System.out.println("Літак:");
+        vehicle1.displayInfo();
+        System.out.println("\nАвтомобіль:");
+        vehicle2.displayInfo();
+        System.out.println("\nКорабель:");
+        vehicle3.displayInfo();
     }
 }
-
-
-
-
-
--- src/myArea.java
+++
@@ -1,9 +0,0 @@
-public class myArea {
-    public static final double PI = 3.14;
-
-    public static double areaOfCircle(double radius) {
-        double area = PI * radius * radius;
-        System.out.println("Площа кола з радіусом " + radius + " дорівнює:");
-        return area;
-    }
-}
