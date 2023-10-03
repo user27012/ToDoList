83014a5e4d6eeb116efd3045e341d568e26a7e5a
user27012 <kolabelozorov@gmail.com>
2023-10-03 22:57:52 +0200

    HW - 05 - 01

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,42 @@
-class Main {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        Printer printer = new Printer();
-        RedPrinter redPrinter = new RedPrinter();
-        BluePrinter bluePrinter = new BluePrinter();
+        Scanner scan = new Scanner(System.in);
+        AbstractHandler xmlHandler = new XMLHandler();
+        AbstractHandler txtHandler = new TXTHandler();
+        AbstractHandler docHandler = new DOCHandler();
 
-        printer.print("Звичайний текст");
-        redPrinter.print("Цей текст червоний");
-        bluePrinter.print("Цей текст синій");
+        System.out.println("Добро пожаловать! О чем я мог бы вам рассказать? Нажмите 1 что бы просмотреть меню.");
+        int change = scan.nextInt();
+        if (change == 1){
+            System.out.println("Что делает XMLHandler = 3 \nЧто делает TXTHandler = 2 \nЧто делает DOCHandler = 1");
+            int then = scan.nextInt();
+            switch (then){
+                case 3:
+                    xmlHandler.open();
+                    xmlHandler.create();
+                    xmlHandler.save();
+                    xmlHandler.change();
+                    break;
+                case 2:
+                    txtHandler.change();
+                    txtHandler.save();
+                    txtHandler.open();
+                    txtHandler.create();
+                    break;
+                case 1:
+                    docHandler.create();
+                    docHandler.open();
+                    docHandler.save();
+                    docHandler.change();
+                default:
+                    System.out.println("Элемент не расспознан...");
+                    break;
+            }
 
-        // Використання приведення типів
-        Printer redAsPrinter = (Printer) redPrinter;
-        redAsPrinter.print("Цей текст червоний (приведення до Printer)");
-
-        Printer blueAsPrinter = (Printer) bluePrinter;
-        blueAsPrinter.print("Цей текст синій (приведення до Printer)");
+        } else {
+            System.out.println("К вводу доступны только 1 или ничего.");
+        }
     }
 }
