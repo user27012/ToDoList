fc981154db8d37f338d1dff03bc2a09829f1c0dd
Nickolay Graysenko <86128973+user27012@users.noreply.github.com>
2023-10-19 22:46:49 +0200

    Delete src/Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Animals cat = Animals.MAVPA;
-        Animals dog = Animals.VOVK;
-
-        System.out.println(cat);
-        System.out.println(dog);
-    }
-}
