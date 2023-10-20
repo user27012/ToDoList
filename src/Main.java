ab4ed685e3d771140e9506b05b3e67130c6c0e26
user27012 <kolabelozorov@gmail.com>
2023-10-20 21:29:16 +0200

    HW - 07 - 02

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        MyList myList = new MyList();
+        MyDictionary dictionary = new MyDictionary();
 
+        dictionary.Dobavit("Сегодня было солнечтно");
+        dictionary.Dobavit("Вчера было туманно");
+        dictionary.Dobavit("Позавчера шел дождь");
 
-        myList.addElement(10);
-        myList.addElement(20);
-        myList.addElement(30);
+        System.out.println(dictionary.getElement(0));
+        System.out.println(dictionary.getElement(1));
+        System.out.println(dictionary.getElement(2));
 
-        System.out.println("Елемент с индексом 1: " + myList.getElement(1));
-        System.out.println("Общее количество индексов: " + myList.getCount());
+        System.out.println("Всего записей: " + dictionary.getSize());
     }
 }
